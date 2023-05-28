db7fb3aef72efa2563d84a831bc18b828e94c947
Radina Ovcharova <93722607+radi676@users.noreply.github.com>
2023-05-28 14:08:02 +0300

    Update JsonElement with SharedPtr qualities

--- Elements/JsonElement.h
+++ Elements/JsonElement.h
@@ -5,7 +5,6 @@
 #include "../Utils/Output/Output.h"
 #include "../Utils/SharedPtr/SharedPtr.hpp"
 
-// TODO:Big 4
 class JsonElement
 {
 protected:
@@ -18,26 +17,29 @@ public:
     JsonElement() : JsonElement(JsonElementType::ValueElement, nullptr) {}
     JsonElement(const JsonElementBase &value) : JsonElement(JsonElementType::ValueElement, value.clone()) {}
 
-    // it can't be keyed value element
-    
     virtual void print(std::ostream &o, size_t inset = 0, bool shouldInset = true) const
     {
         printInset(o, shouldInset ? inset : 0);
         _value->print(o, inset);
     }
 
-    JsonElementType getType() const
+    void setValue(JsonElementBase *ptr)
     {
-        return type;
+        _value = SharedPtr<JsonElementBase>(ptr);
     }
 
-    JsonElementBase *&value()
+    void setValue(const SharedPtr<JsonElementBase> &ptr)
     {
-        return _value.ptr();
+        _value = ptr;
     }
 
-    const JsonElementBase *value() const
+    SharedPtr<JsonElementBase> &value()
     {
-        return _value.ptr();
+        return _value;
+    }
+
+    const SharedPtr<JsonElementBase> &value() const
+    {
+        return _value;
     }
 };
